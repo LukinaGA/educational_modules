5b98bf8bda6643448e8b3d0e1ade71088321da99
Galina Lukina <lukina.galina11@gmail.com>
2025-03-12 23:42:25 +0400

    add model EducationalModule

--- educational_modules/models.py
+++ educational_modules/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class EducationalModule(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Образовательный модуль", help_text="Введите название модуля")
+    description = models.TextField(verbose_name="Описание образовательного модуля", help_text="Введите описание модуля")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец",
+                              help_text="Выберите владельца модуля", related_name="modules", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Образовательный модуль"
+        verbose_name_plural = "Образовательные модули"
+
+    def __str__(self):
+        return self.name
