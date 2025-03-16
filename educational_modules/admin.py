f5cc238aa791260ac91066c958857c1919f8d255
Galina Lukina <lukina.galina11@gmail.com>
2025-03-16 15:46:14 +0400

    register EducationalModule, Topic and Lesson

--- educational_modules/admin.py
+++ educational_modules/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from educational_modules.models import EducationalModule, Topic, Lesson
+
+
+@admin.register(EducationalModule)
+class EducationalModuleAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "topic", "owner", "video_link")
+    search_fields = ("name",)
+    ordering = ("name",)
