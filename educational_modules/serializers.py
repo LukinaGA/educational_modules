769b93237bb625bf8d5f2ac88664101490d48c8b
Galina Lukina <lukina.galina11@gmail.com>
2025-03-16 16:57:05 +0400

    add output of information about topics and lessons

--- educational_modules/serializers.py
+++ educational_modules/serializers.py
@@ -14,14 +14,24 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class TopicSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
     class Meta:
         model = Topic
-        fields = ["id", "name", "description", "educational_module"]
+        fields = ["id", "name", "description", "educational_module", "lessons_count", "lessons"]
 
 
 class EducationalModuleSerializer(serializers.ModelSerializer):
+    topics_count = serializers.SerializerMethodField(read_only=True)
+    topics = TopicSerializer(many=True, read_only=True)
+
+    def get_topics_count(self, obj):
+        return obj.topics.count()
 
     class Meta:
         model = EducationalModule
-        fields = ["id", "name", "description"]
+        fields = ["id", "name", "description", "topics_count", "topics"]
